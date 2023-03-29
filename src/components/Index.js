28fe5f4dcd5ef62c3b8e772d6d7c983f8b99b360
naila shah <naila.shah1208@gmail.com>
2023-03-29 22:24:06 +0530

    commit all files

---
+++ src/components/Index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Index() {
+  return (
+    <div>
+    <nav>
+    <span>Logo</span>   
+    <div>
+    <ul>
+    <li>Home</li>   
+    <li>About</li> 
+    <li>Contact</li>
+    </ul>    
+    </div> 
+    </nav>  
+    <div>
+    <div className="card"></div>    
+    </div>
+    </div>
+  )
+}
+
+export default Index
